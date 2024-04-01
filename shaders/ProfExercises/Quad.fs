351684b8c6798316f6ec6f749236ca0148981a58
Betsy Kamas <kamase@sunypoly.edu>
2024-04-01 17:54:59 -0400

    Class March 28 FBO code

---
+++ shaders/ProfExercises/Quad.fs
@@ -0,0 +1,11 @@
+#version 410 core
+
+layout(location=0) out vec4 out_color;
+
+in vec2 interUV;
+
+uniform sampler2D screenTexture;
+
+void main(){
+    out_color = texture(screenTexture, interUV);
+}
